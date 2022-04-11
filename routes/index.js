b9d06ce76f8874cfd1026a5ec5e6f91e6bc00211
abderrahman bensalah <bensalahabdelrrahman@gmail.com>
2022-04-11 22:36:01 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var data = require('../data/data.json')
+JSON.stringify(data)
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 'users': data });
+});
+
+
+router.get('/:id', function (req, res) {
+  const id = req.params.id
+  if(id < data.length){
+    res.render('index', { 'users': data ,'id':id});
+    console.log('true')
+  }
+  else{
+    res.render('index', { 'users': data ,'id':-1})
+    console.log('false')
+  }
+})
+
+module.exports = router;
